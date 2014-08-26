feb44de2898cf44084d57b3aaa3c4509067f7119
DFJS <dshewry@dfjs.co.uk>
2014-08-26 11:20:20 +0100

    create play 2.2.2 version

--- play22.rb
+++ play22.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Play22 < Formula
   homepage "http://www.playframework.org/"
-  url "http://downloads.typesafe.com/play/2.2.3/play-2.2.3.zip"
-  sha1 "16beea55568a6b5876439ffbf908ba6448c5c713"
+  url "http://downloads.typesafe.com/play/2.2.2/play-2.2.2.zip"
+  sha1 "9a2fa3c6b9ee36375d814d775bec4335e427dcd2"
 
   conflicts_with "sox", :because => "both install `play` binaries"
 
